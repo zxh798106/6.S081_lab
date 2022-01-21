06962c8b3f1654d0f9f113efc830c012496cc8bc
zxh <1098754443@qq.com>
2022-01-21 23:33:52 +0800

    Buffer cache实验代码完成。不知道为什么只有第一次usertests执行成功。

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,6 +23,14 @@
 #include "fs.h"
 #include "buf.h"
 
+#define NBUC 13
+
+// 哈希结构体
+struct {
+  struct spinlock lock;
+  struct buf head;
+} bhash[NBUC];
+
 struct {
   struct spinlock lock;
   struct buf buf[NBUF];
@@ -30,7 +38,7 @@ struct {
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
+  //struct buf head;
 } bcache;
 
 void
@@ -41,15 +49,22 @@ binit(void)
   initlock(&bcache.lock, "bcache");
 
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
+  /*bcache.head.prev = &bcache.head;
+  bcache.head.next = &bcache.head;*/
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
+    /*b->next = bcache.head.next;
+    b->prev = &bcache.head;*/
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    /*bcache.head.next->prev = b;
+    bcache.head.next = b;*/
   }
+
+  // 初始化bhash数组元素的锁和哈希链表
+  for (int i = 0; i < NBUC; ++i) {
+    initlock(&bhash[i].lock, "bhash");
+    bhash[i].head.prev = &bhash[i].head;
+    bhash[i].head.next = &bhash[i].head;
+  }  
 }
 
 // Look through buffer cache for block on device dev.
@@ -60,7 +75,71 @@ bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-  acquire(&bcache.lock);
+  // 在哈希表中查找block
+  int idx = blockno % NBUC;
+  acquire(&bhash[idx].lock);
+  for (b = bhash[idx].head.next; b != &bhash[idx].head; b = b->next) {
+    if (b->dev == dev && b->blockno == blockno) {
+      b->refcnt++;
+      b->ticks = ticks;
+      release(&bhash[idx].lock);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+
+  struct buf *LRU_b = 0;
+  acquire(&bcache.lock); // bcache.lock不能去掉，去掉了usertests过不去。
+  // 若哈希表中没有目标block，寻找ticks最小的block
+  for (b = bcache.buf; b < bcache.buf + NBUF; b++) {
+    if (b->refcnt == 0) {
+      if (!LRU_b) {
+        LRU_b = b;
+      }
+      else if (b->ticks < LRU_b->ticks) {
+        LRU_b = b;
+      }
+    }
+  }
+  
+  if (LRU_b) {
+    int LRU_idx = LRU_b->blockno % NBUC;
+    if (LRU_idx != idx) {
+      acquire(&bhash[LRU_idx].lock);
+      // 将旧的block从原哈希表中移除
+      if (LRU_b->prev)
+        LRU_b->prev->next = LRU_b->next;
+      if (LRU_b->next)
+        LRU_b->next->prev = LRU_b->prev;
+      // 将新的block插入到哈希表中
+      LRU_b->prev = &bhash[idx].head;
+      LRU_b->next = bhash[idx].head.next;
+      bhash[idx].head.next->prev = LRU_b;
+      bhash[idx].head.next = LRU_b;
+
+      LRU_b->dev = dev;
+      LRU_b->blockno = blockno;
+      LRU_b->valid = 0;
+      LRU_b->refcnt = 1;
+      LRU_b->ticks = ticks;
+      release(&bhash[LRU_idx].lock); // 考虑一下这两个idx的锁无序的话，是否会死锁。
+    }
+    else {
+      LRU_b->dev = dev;
+      LRU_b->blockno = blockno;
+      LRU_b->valid = 0;
+      LRU_b->refcnt = 1;
+      LRU_b->ticks = ticks;
+    }
+    release(&bcache.lock);
+    release(&bhash[idx].lock);
+    acquiresleep(&LRU_b->lock);
+    return LRU_b;
+  }
+  else 
+    panic("bget: no buffers");
+
+  /*acquire(&bcache.lock);
 
   // Is the block already cached?
   for(b = bcache.head.next; b != &bcache.head; b = b->next){
@@ -85,7 +164,7 @@ bget(uint dev, uint blockno)
       return b;
     }
   }
-  panic("bget: no buffers");
+  panic("bget: no buffers");*/
 }
 
 // Return a locked buf with the contents of the indicated block.
@@ -121,7 +200,15 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  int idx = b->blockno % NBUC;
+  acquire(&bhash[idx].lock);
+  //acquire(&bcache.lock);
+  b->refcnt--;
+  //b->ticks = ticks; // csdn代码注释了，我觉得不应该注释。结果谁注释了跑出来的。 
+  release(&bhash[idx].lock);
+  //release(&bcache.lock);
+
+  /*acquire(&bcache.lock);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
@@ -133,21 +220,31 @@ brelse(struct buf *b)
     bcache.head.next = b;
   }
   
-  release(&bcache.lock);
+  release(&bcache.lock);*/
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int idx = b->blockno % NBUC;
+  acquire(&bhash[idx].lock);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bhash[idx].lock);
+
+  /*acquire(&bcache.lock);
+  b->refcnt++;
+  release(&bcache.lock);*/
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int idx = b->blockno % NBUC;
+  acquire(&bhash[idx].lock);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bhash[idx].lock);
+  
+  /*acquire(&bcache.lock);
+  b->refcnt--;
+  release(&bcache.lock);*/
 }
 
 
